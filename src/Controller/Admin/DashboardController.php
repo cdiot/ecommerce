a2fdbbe759f7c084c3cd45f627fbc42955d3008a
Chris <chris2001@orange.fr>
2021-11-24 18:51:04 +0100

    Add list of all carrier in dashboard of EasyAdmin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -6,6 +6,7 @@ use App\Entity\Carrier;
 use App\Entity\Category;
 use App\Entity\Product;
 use App\Entity\User;
+use App\Repository\CarrierRepository;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
 use App\Repository\UserRepository;
@@ -22,11 +23,16 @@ class DashboardController extends AbstractDashboardController
     protected $productRepository;
 
 
-    public function __construct(UserRepository $userRepository, CategoryRepository $categoryRepository, ProductRepository $productRepository)
-    {
+    public function __construct(
+        UserRepository $userRepository,
+        CategoryRepository $categoryRepository,
+        ProductRepository $productRepository,
+        CarrierRepository $carrierRepository
+    ) {
         $this->userRepository = $userRepository;
         $this->categoryRepository = $categoryRepository;
         $this->productRepository = $productRepository;
+        $this->carrierRepository = $carrierRepository;
     }
 
     #[Route('/admin', name: 'admin')]
@@ -35,7 +41,8 @@ class DashboardController extends AbstractDashboardController
         return $this->render('bundles/EasyAdminBundle/welcome.html.twig', [
             'countAllUser' => $this->userRepository->countAllUser(),
             'categories' => $this->categoryRepository->findAll(),
-            'countAllProduct' => $this->productRepository->countAllProduct()
+            'countAllProduct' => $this->productRepository->countAllProduct(),
+            'carriers' => $this->carrierRepository->findAll()
         ]);
     }
 
