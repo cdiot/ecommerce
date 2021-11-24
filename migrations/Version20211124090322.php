a53c3ddb89c8cd08bc9f4fc57c6beb9c8867c483
Chris <chris2001@orange.fr>
2021-11-24 10:13:58 +0100

    Add Address entity + test

---
+++ migrations/Version20211124090322.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211124090322 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add Address entity';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE address (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, name VARCHAR(30) NOT NULL, firstname VARCHAR(50) NOT NULL, lastname VARCHAR(50) NOT NULL, company VARCHAR(50) DEFAULT NULL, address VARCHAR(100) NOT NULL, postal VARCHAR(30) NOT NULL, city VARCHAR(50) NOT NULL, country VARCHAR(50) NOT NULL, phone VARCHAR(30) NOT NULL, INDEX IDX_D4E6F81A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE address ADD CONSTRAINT FK_D4E6F81A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE address');
+    }
+}
