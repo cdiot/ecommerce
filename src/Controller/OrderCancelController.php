283df7a6db4c31c0266f9b6a1f7d7c7cd431b29f
Chris <chris2001@orange.fr>
2021-12-02 19:08:37 +0100

    Add Mailer in order error controller

--- src/Controller/OrderCancelController.php
+++ src/Controller/OrderCancelController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\OrderRepository;
+use App\Service\Mailer;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,13 +12,23 @@ use Symfony\Component\Routing\Annotation\Route;
 class OrderCancelController extends AbstractController
 {
     #[Route('/commande/erreur/{stripeSessionId}', name: 'order_cancel')]
-    public function index(OrderRepository $orderRepository, $stripeSessionId): Response
+    public function index(OrderRepository $orderRepository, Mailer $mailer, $stripeSessionId): Response
     {
         $order = $orderRepository->findOneByStripeSessionId($stripeSessionId);
 
         if (!$order || $order->getUser() != $this->getUser()) {
             return $this->redirectToRoute('/');
         }
+
+        $mailer->send(
+            'Erreur commande #' .  $order->getReference() . ' - Ecommerce | SITE OFFICIEL',
+            'bar@ecommerce.com',
+            $order->getUser()->getEmail(),
+            'emails/order_cancel.html.twig',
+            [
+                'order' => $order,
+            ]
+        );
 
         return $this->render('order_cancel/index.html.twig', [
             'order' => $order
