540a27a2ccc6bedfd5e1ed0f93aa1c989cd2a019
Chris <chris2001@orange.fr>
2021-12-04 12:01:56 +0100

    Add Mailer in contact controller

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Form\ContactType;
+use App\Service\Mailer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,14 +12,25 @@ use Symfony\Component\Routing\Annotation\Route;
 class ContactController extends AbstractController
 {
     #[Route('/nous-contacter', name: 'contact')]
-    public function index(Request $request)
+    public function index(Request $request, Mailer $mailer)
     {
         $form = $this->createForm(ContactType::class);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
             $this->addFlash('notice', 'Notre équipe, vous répondrons dans les meilleurs délais.');
+            $mailer->send(
+                $data['subject'],
+                'bar@ecommerce.com',
+                $data['email'],
+                'emails/contact.html.twig',
+                [
+                    'content' => $data['content'],
+                ]
+            );
+            return $this->redirectToRoute('contact');
         }
 
         return $this->render('contact/index.html.twig', [
