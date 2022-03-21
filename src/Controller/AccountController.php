caedac03cf7a768158147b92bac75a691ce3d87d
Chris <chris2001@orange.fr>
2022-03-21 13:43:08 +0100

    Add IsGranted('IS_AUTHENTICATED_FULLY')] in path /account

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[IsGranted('IS_AUTHENTICATED_FULLY')]
 class AccountController extends AbstractController
 {
     #[Route(path: [
--- src/Controller/AccountOrderController.php
+++ src/Controller/AccountOrderController.php
@@ -4,10 +4,12 @@ namespace App\Controller;
 
 use App\Entity\Order;
 use App\Repository\OrderRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[IsGranted('IS_AUTHENTICATED_FULLY')]
 class AccountOrderController extends AbstractController
 {
     #[Route(path: [
