d3d60d9e182bac5eaec1af40c9c8bb882263f6bc
Chris <chris2001@orange.fr>
2022-03-21 13:43:41 +0100

    Add IsGranted('IS_AUTHENTICATED_FULLY')] in path /order

--- src/Controller/OrderCancelController.php
+++ src/Controller/OrderCancelController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\OrderRepository;
 use App\Service\Mailer;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,6 +17,7 @@ class OrderCancelController extends AbstractController
         'en' => '/order/error/{stripeSessionId}',
         'fr' => '/commande/erreur/{stripeSessionId}'
     ], name: 'order_cancel')]
+    #[IsGranted('ORDER_VIEW', subject: 'orderRepository')]
     public function index(OrderRepository $orderRepository, Mailer $mailer, $stripeSessionId, TranslatorInterface $translator): Response
     {
         $order = $orderRepository->findOneByStripeSessionId($stripeSessionId);
--- src/Controller/OrderSuccessController.php
+++ src/Controller/OrderSuccessController.php
@@ -6,11 +6,13 @@ use App\Service\Cart;
 use App\Repository\OrderRepository;
 use App\Service\Mailer;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
+#[IsGranted('IS_AUTHENTICATED_FULLY')]
 class OrderSuccessController extends AbstractController
 {
     public function __construct(private EntityManagerInterface $entityManager)
